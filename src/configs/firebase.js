12be37f7a330f6c5218a56b1b041ad052186a9b0
Ualas Ribeiro <ualasr.santos@gmail.com>
2022-11-25 07:45:25 -0300

    Initial config
    Instalação inicial e ajuste.

---
+++ src/configs/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBFfHl3vsYWoTgF79u4tHXTt5E4KqaOf-c",
+
+  authDomain: "expert-reports-dbb24.firebaseapp.com",
+
+  projectId: "expert-reports-dbb24",
+
+  storageBucket: "expert-reports-dbb24.appspot.com",
+
+  messagingSenderId: "947022043453",
+
+  appId: "1:947022043453:web:5fcaa030d3b9165aa94d54"
+
+};
+
+const app = initializeApp(firebaseConfig);
